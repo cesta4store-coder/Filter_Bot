a0ddfe2e50834de1707a008b38b200289c407ef5
cesta4store-coder <cesta.4.store@gmail.com>
2025-11-04 10:33:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
-API_ID = int(environ.get('API_ID', '')) # API ID from my.telegram.org
-API_HASH = environ.get('API_HASH', '')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "")    # Bot token from @BotFather
+API_ID = int(environ.get('API_ID', '21575196')) # API ID from my.telegram.org
+API_HASH = environ.get('API_HASH', 'ed2748ad0d2844020703b538f20f7518')  # API Hash from my.telegram.org
+BOT_TOKEN = environ.get('BOT_TOKEN', "8513386358:AAEQcWEIfeLSDlm9QBVB3zjDYib7IgxSJyw")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
